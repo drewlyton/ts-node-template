4f6951dfae8860b620a16e755e50d7b041a19a46
Drew Lytle <aml3ed@virginia.edu>
2022-11-14 17:10:22 -0500

    Initial commit

---
+++ src/index.test.ts
@@ -0,0 +1,3 @@
+it('Hello world', () => {
+  expect(true).toBeTruthy()
+})
---
+++ vitest.config.ts
@@ -0,0 +1,13 @@
+/// <reference types="vitest" />
+/// <reference types="vite/client" />
+
+import { defineConfig } from 'vitest/config'
+
+export default defineConfig({
+  test: {
+    environment: 'node',
+    globals: true,
+    exclude: ['./node_modules'],
+    watchExclude: ['.*\\/node_modules\\/.*', '.*\\/build\\/.*'],
+  },
+})
